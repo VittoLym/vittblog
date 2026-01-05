48e2882532caec94ef8eb16ef8148a0ad1828285
vittolym <lexymdz@gmail.com>
2026-01-05 20:39:43 -0300

    metatag & some SEO

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue"
+import { createHead } from "@vueuse/head"
 import App from "./App.vue"
 import router from "./router"
 
+const head = createHead()
 createApp(App)
   .use(router)
+  .use(head)
   .mount("#app")
