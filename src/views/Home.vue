220ecb5dda0069f84a462db1028a21621449c96a
vittolym <lexymdz@gmail.com>
2026-01-11 16:22:21 -0300

    fixing that

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -167,7 +167,7 @@ onUnmounted(() => clearInterval(interval))
   backdrop-filter: blur(3px);
   box-shadow: 0 8px 32px rgba(0, 0, 0, 0), inset 0 1px 0 rgba(255, 255, 255, 0.15);
   color: white;
-  height: 45vh;
+  height: 40vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
