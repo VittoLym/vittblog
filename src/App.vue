6356a3879d0b6b031ffa4ec035770e4b836d1fef
vittolym <lexymdz@gmail.com>
2026-01-21 18:07:07 -0300

    litle bug

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@ function mobileState(){
         <nav class="sections">
           <router-link to="/">Home</router-link>
           <router-link to="/blogs">Blogs</router-link>
-          <router-link to="/test">Crear</router-link>
+          <router-link to="/article/new">Crear</router-link>
         </nav>
         <aside class="buttons">
           <nav class="nav" >
